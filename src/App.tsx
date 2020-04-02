6191a531804bfcaca84d70901c8403524ebd42dd
danielwhitford <whitford.dsw@gmail.com>
2020-04-02 01:22:36 +0100

    more word types

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,111 @@
 import React, { useState } from 'react';
+import WordChip from './WordChip';
+import SimplePos from './SimplePos';
+import './global.css'
 
-type poo = [string, string]
+type TaggedWords = [string, SimplePos]
+type Toggle = [string, SimplePos, boolean]
+
+const getColour = (pos: SimplePos): string => {
+  switch(pos){
+    case (SimplePos.Adjective): {
+      return '#96ceb4'
+    }
+    case (SimplePos.Adverb): {
+      return '#ffeead'
+    }
+    case (SimplePos.Noun): {
+      return '#ffcc5c'
+    }
+    case (SimplePos.Pronoun): {
+      return '#ff6f69'
+    }
+    case (SimplePos.Verb): {
+      return '#588c7e'
+    }
+    case (SimplePos.Conjunctives): {
+      return '#f2e394'
+    } 
+    case (SimplePos.Determiner): {
+      return '#f2ae72'
+    } 
+    case (SimplePos.Preposition): {
+      return '#d96459'
+    }
+    default: {
+      return '#43464B'
+    }
+  }
+}
 
 function App() {
-  const x: poo[] = []
-  const [tagged, setTagged] = useState(x)
+  const [tagged, setTagged] = useState([] as TaggedWords[])
+  const [posToggles, setPosToggles] = useState(Object
+    .entries(SimplePos)
+    .map(([label, pos]) => [label, pos, true] as Toggle)
+  )
 
   const change = () => {
     const inputArea = document.getElementById('input-area') as HTMLInputElement
-    fetch('http://postaggr-env.eba-pnma8zwt.eu-west-1.elasticbeanstalk.com/simple/',
-    {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        'words': inputArea.value
+    fetch('http://localhost:8081/simple/',
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'words': inputArea.value
+        })
       })
-    })
       .then(res => res.json())
-      .then(j =>setTagged(j))
+      .then(j => setTagged(j))
   }
 
   return (
     <div>
-      <textarea id='input-area' onBlur={change}></textarea>
+      <textarea id='input-area' onBlur={change} defaultValue='Hello I am Dan and I am writing dummy text.' cols={80} rows={10}></textarea>
       <br />
       <button onClick={change}>Tag</button>
       <br />
       <div >
-        {tagged.map(t => <span className={t[1]}>{t[0]} </span>)}
+        {tagged.map(([word, pos], i) => (
+          <WordChip
+            key={i}
+            word={word}
+            type={pos}
+            visible={posToggles.find(([, p]) => p === pos)?.[2]}
+            colour={getColour(pos)}
+          />          
+        ))}
+      </div>
+      <div>
+        {posToggles.map(([label, pos, toggled], i) => (
+          <div key={i}>
+            <div
+              style={{
+                height: '10px',
+                width: '10px',
+                backgroundColor: getColour(pos),
+                display: 'inline-block'
+              }}
+            ></div>
+            <input type="checkbox" id={label} name={label} value={label} checked={toggled}
+              onChange={() => {
+                const pt = posToggles.map(([l, p, t]) => {
+                  if (p === pos) {
+                    return [l, p, !t] as Toggle
+                  } else {
+                    return [l, p, t] as Toggle
+                  }                  
+                })
+                setPosToggles(pt)
+              }}
+            />
+            <label>{label}</label>
+            <br></br>
+          </div>
+        ))}
       </div>
     </div>
   );
