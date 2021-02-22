305e937d5ba24d188332409ee1ffeb2ae302054e
shaftoe44 <shaftoe44@users.noreply.github.com>
2021-02-22 19:18:57 +0000

    Deploying to gh-pages from @ shaftoe44/taggr-ui@653cb43a58a403d374561ff8e0287b779a40fbde 🚀

---
+++ precache-manifest.5aa1b87cdea758710ab9e89284d0ba68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2fca7b6481610864a09a9709bae73e",
+    "url": "/taggr-ui/index.html"
+  },
+  {
+    "revision": "4b313641b816fde643b8",
+    "url": "/taggr-ui/static/css/main.947555df.chunk.css"
+  },
+  {
+    "revision": "13b5dd860d233f2fbb9a",
+    "url": "/taggr-ui/static/js/2.0a64dfa1.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/taggr-ui/static/js/2.0a64dfa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b313641b816fde643b8",
+    "url": "/taggr-ui/static/js/main.c6df9b87.chunk.js"
+  },
+  {
+    "revision": "053ffb2e16e1f2007050",
+    "url": "/taggr-ui/static/js/runtime-main.8cfd42eb.js"
+  }
+]);
